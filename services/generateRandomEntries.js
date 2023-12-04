db5f79675a7d4d31c5254ecdb9f79f9c930d460d
Helios <helios.yamet@gmail.com>
2023-12-05 03:27:03 +0900

    Update generateRandomEntries.js
    Co-Authored-By: YametDev <131508458+YametDev@users.noreply.github.com>

--- services/generateRandomEntries.js
+++ services/generateRandomEntries.js
@@ -2,13 +2,6 @@ const bcrypt = require('bcryptjs')
 const { ACTIVITY, ROLE, LOCATION, BRANCHES } = require('../constants/constant')
 const Signup = require('../models/signup')
 
-const branches = [
-  'Parul University',
-  'Vadodara Startup Studio',
-  'Ahmedabad Startup Studio',
-  'Rajkot Startup Studio',
-  'Surat Startup Studio',
-];
 module.exports = {
   eventAndMeetings,
 }
