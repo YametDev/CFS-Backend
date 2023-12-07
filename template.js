fc433b87d80881bc46f3542ae75697fb2d5961f4
YametDev <heliyamet3190@gmail.com>
2023-12-07 17:39:16 +0900

    fixed: email sender

--- template.js
+++ template.js
@@ -20,7 +20,7 @@ module.exports.templateEmail = (val) => {
   } Quality of Products</p>`
 
   const contact =
-    (val.name.length ?? 0) * (val.email.length ?? 0) * (val.phone.length ?? 0)
+    val.name?.length || val.email?.length || val.phone?.length
       ? '<br/><p><b>Customer Contacts:</b></p>'
       : ''
   const name = val.name?.length ? `<p>${val.name}</p>` : ''
